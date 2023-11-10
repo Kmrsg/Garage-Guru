d2bedb121d796ad9c79d736db661905a2c9b95b9
Sergei Komarov <komarovs87@mail.ru>
2023-11-10 11:22:13 +0300

    fixiki

--- server/routes/api/uslugaPrice.api.routes.js
+++ server/routes/api/uslugaPrice.api.routes.js
@@ -1,30 +1,30 @@
-const router = require("express").Router();
-const { UslugaPrice } = require("../../db/models");
-const { Order } = require("../../db/models");
-const { User } = require("../../db/models");
-const { Usluga } = require("../../db/models");
-const { CarModel } = require("../../db/models");
-const { Mark } = require("../../db/models");
-const { OrderItem } = require("../../db/models");
+const router = require('express').Router();
+const { UslugaPrice } = require('../../db/models');
+const { Order } = require('../../db/models');
+const { User } = require('../../db/models');
+const { Usluga } = require('../../db/models');
+const { CarModel } = require('../../db/models');
+const { Mark } = require('../../db/models');
+const { OrderItem } = require('../../db/models');
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    // console.log(req.session.serviceId);
-    const orders = await UslugaPrice.findAll({
-      where: { service_id: req.session.serviceId },
+    if (req.session.serviceId) {
+      const orders = await UslugaPrice.findAll({
+        where: { service_id: req.session.serviceId },
 
-      include: [
-        { model: Usluga },
-        { model: CarModel },
-        { model: Mark },
-        {
-          model: OrderItem,
-          include: { model: Order, include: { model: User } },
-        },
-      ],
-    });
-    // console.log(orders);
-    res.json(orders);
+        include: [
+          { model: Usluga },
+          { model: CarModel },
+          { model: Mark },
+          {
+            model: OrderItem,
+            include: { model: Order, include: { model: User } },
+          },
+        ],
+      });
+      res.json(orders);
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json(error);
